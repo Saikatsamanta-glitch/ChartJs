9c219307e3db9dd8efc5b0afce0b58e34bac1ffb
saikat samanta <saikatsamanta052@gmail.com>
2023-09-21 15:22:46 +0530

    hanges in graph

--- src/App.js
+++ src/App.js
@@ -4,9 +4,30 @@ import Chart from 'chart.js/auto';
 import covidSheet from './covid-19 states current.xlsx';
 import { read, utils, writeFile } from 'xlsx';
 import pattern from 'patternomaly'
-import './index.css'
+import './index.css';
+
+function createDiagonalPattern(color = 'black') {
+        let shape = document.createElement('canvas')
+        shape.width = 10
+        shape.height = 10
+        let c = shape.getContext('2d')
+        c.strokeStyle = color
+        c.beginPath()
+        c.moveTo(2, 0)
+        c.lineTo(10, 8)
+        c.stroke()
+        c.beginPath()
+        c.moveTo(0, 8)
+        c.lineTo(2, 10)
+        c.stroke()
+        return c.createPattern(shape, 'repeat')
+      }
+
 function App() {
         const [pres, setPres] = useState([]);
+        
+        const danger = createDiagonalPattern('red');
+        const safe = createDiagonalPattern('green');
         const [x,setX] =useState([]);
         const [y,setY] =useState([])
         /* Fetch and update the state once */
@@ -18,11 +39,23 @@ function App() {
                         const ws = wb.Sheets[wb.SheetNames[0]]; // get the first worksheet
                         const data = utils.sheet_to_json(ws); // generate objects
                         setPres(data); // update state
-                       
+                        
                 })();
 
                 
         }, []);
+        // new array
+        const bgArray=pres.map((v)=>{
+                if(v.positive > 8000 && v.positive <= 10000 ){
+                        return danger
+                }else if(v.positive > 10000 ){
+                        return 'white'
+                } else{
+                        return safe
+                }
+        })
+        
+        console.log(bgArray);
         const state = {
                 // x axis
                 labels: pres.map((val)=> val.state ),
@@ -30,9 +63,9 @@ function App() {
                 datasets: [
                         {
                                 label: 'Rainfall',
-                                backgroundColor: pattern.draw('diagonal', '#1f77b4'),
-                                borderColor: 'rgba(0,0,0,1)',
+                                backgroundColor: bgArray ,
                                 borderWidth: 2,
+                                
                                 //     y axis
                                 data: pres.map(val=>val.positive)
                         }
